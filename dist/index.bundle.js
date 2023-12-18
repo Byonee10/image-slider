69d0737208f8cab058ff399dbab26ce1b677f8fb
Ramazan Çağlar Bulgay <65749103+Byonee10@users.noreply.github.com>
2023-12-18 18:29:59 +0300

    Initial commit

---
+++ dist/index.bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const div = document.createElement("div");
+div.innerHTML = "Hello world";
+
+document.body.appendChild(div);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTs7QUFFQSwrQiIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG9saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGRpdiA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoXCJkaXZcIik7XG5kaXYuaW5uZXJIVE1MID0gXCJIZWxsbyB3b3JsZFwiO1xuXG5kb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGRpdik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
